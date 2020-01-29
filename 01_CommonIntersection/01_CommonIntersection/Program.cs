9ddafe5429e0212942b6ac069853bdea5d1d7cb5
False Author <FalseAuthor@protonmail.com>
2020-01-29 17:46:08 +0800

    second

--- 01_CommonIntersection/01_CommonIntersection/Program.cs
+++ 01_CommonIntersection/01_CommonIntersection/Program.cs
@@ -10,7 +10,56 @@ namespace _01_CommonIntersection
     {
         static void Main(string[] args)
         {
+            var appCollection = new appliances[0];
+            var flag = true;
+            while(flag)
+            {
+                Console.WriteLine("\n-----------OPTIONS-------------");
+                Console.WriteLine("Enter 1 - to Add Appliance");
+                Console.WriteLine("Enter 2 - to View Collection");
+                Console.WriteLine("Enter 3 - to View Common Intervals");
+                Console.WriteLine("Enter x - to EXIT");
+
+                Console.Write(">>> ");
+                var input = Console.ReadLine();
+                switch (input)
+                {
+                    case "1":
+
+                        break;
+                    case "2":
+                        break;
+                    case "3":
+                        break;
+                    case "x": flag = false; break;
+                }
+            }
+
             Console.Read();
         }
+
+        static appliances[] addAppliances(appliances[] collection, appliances item)
+        {
+            Array.Resize(ref collection, collection.Length + 1);
+            collection[collection.Length - 1] = item;
+            return collection;
+        }
+    }
+
+    public struct appliances
+    {
+        public string applianceName;
+        public appInterval[] appIntervals;
+    }
+
+    public struct appInterval
+    {
+        public appUsedHour[] appUsedHours;
+    }
+
+    public struct appUsedHour
+    {
+        public int start;
+        public int end;
     }
 }
