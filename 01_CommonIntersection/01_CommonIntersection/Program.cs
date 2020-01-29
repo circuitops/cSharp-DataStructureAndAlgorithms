37cc44d066d5b0ac3d290d8cc0fcced449d0670e
False Author <FalseAuthor@protonmail.com>
2020-01-29 19:07:15 +0800

    third

--- 01_CommonIntersection/01_CommonIntersection/Program.cs
+++ 01_CommonIntersection/01_CommonIntersection/Program.cs
@@ -14,24 +14,32 @@ namespace _01_CommonIntersection
             var flag = true;
             while(flag)
             {
-                Console.WriteLine("\n-----------OPTIONS-------------");
-                Console.WriteLine("Enter 1 - to Add Appliance");
-                Console.WriteLine("Enter 2 - to View Collection");
-                Console.WriteLine("Enter 3 - to View Common Intervals");
-                Console.WriteLine("Enter x - to EXIT");
-
-                Console.Write(">>> ");
-                var input = Console.ReadLine();
-                switch (input)
+                try
                 {
-                    case "1":
+                    Console.WriteLine("\n-----------OPTIONS-------------");
+                    Console.WriteLine("Enter 1 - to Add Appliance and Hours Connected");
+                    Console.WriteLine("Enter 2 - to View Collection");
+                    Console.WriteLine("Enter 3 - to View Common Intervals");
+                    Console.WriteLine("Enter x - to EXIT");
 
-                        break;
-                    case "2":
-                        break;
-                    case "3":
-                        break;
-                    case "x": flag = false; break;
+                    Console.Write(">>> ");
+                    var input = Console.ReadLine();
+                    switch (input)
+                    {
+                        case "1":
+                            appCollection = addAppliances(appCollection, add());
+                            Console.WriteLine("\tAdded");
+                            break;
+                        case "2":
+                            break;
+                        case "3":
+                            break;
+                        case "x": flag = false; break;
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Parse Failed");
                 }
             }
 
@@ -44,19 +52,97 @@ namespace _01_CommonIntersection
             collection[collection.Length - 1] = item;
             return collection;
         }
+        static appliances add()
+        {
+            Console.WriteLine("\n\tADD(1)");
+            Console.Write("\tAppliance Name and Connected Hours: ");
+            var input = Console.ReadLine();
+
+            var name = string.Empty;
+            var start = string.Empty;
+            var end = string.Empty;
+            var connected = new appUsedHour[0];
+            var flag = false;
+            var flagStart = false;
+            var flagEnd = false;
+
+            try
+            {
+                for (int i = 0; i < input.Length; i++)
+                {
+                    if (flag)
+                    {
+                        if (input[i].Equals('{'))
+                        {
+                            flagStart = true;
+                            i++;
+                        }
+                        else if (input[i].Equals(','))
+                        {
+                            flagStart = false;
+                            flagEnd = true;
+                            i++;
+                        }
+                        else if (input[i].Equals('}'))
+                        {
+                            Array.Resize(ref connected, connected.Length + 1);
+                            connected[connected.Length - 1] = new appUsedHour
+                            {
+                                start = int.Parse(start),
+                                end = int.Parse(end)
+                            };
+
+                            start = string.Empty;
+                            end = string.Empty;
+                            flagStart = false;
+                            flagEnd = false;
+                            i++;
+                        }
+
+                        if (flagStart && !input[i].Equals(' '))
+                        {
+                            start += input[i].ToString();
+                        }
+                        if (flagEnd && !input[i].Equals(' '))
+                        {
+                            end += input[i].ToString();
+                        }
+                    }
+                    else
+                    {
+                        if (!input[i].Equals(':'))
+                        {
+                            name += input[i].ToString();
+                        }
+                        else
+                        {
+                            flag = true;
+                        }
+                    }
+                }
+            }
+            catch
+            {
+                throw new Exception();
+            }
+
+            return new appliances
+            {
+                 applianceName = name, appIntervals = connected
+            };
+        }
+        static void view(appliances[] collection)
+        {
+
+        }
     }
 
     public struct appliances
     {
         public string applianceName;
-        public appInterval[] appIntervals;
+        public appUsedHour[] appIntervals;
     }
-
-    public struct appInterval
-    {
-        public appUsedHour[] appUsedHours;
-    }
-
+    
     public struct appUsedHour
     {
         public int start;
