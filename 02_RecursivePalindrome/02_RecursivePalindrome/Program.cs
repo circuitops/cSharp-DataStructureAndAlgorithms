17e90695253fc9b81d93af2d7987bbea8bcca09c
False Author <FalseAuthor@protonmail.com>
2020-02-02 23:14:42 +0800

    Test

---
+++ 02_RecursivePalindrome/02_RecursivePalindrome/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_RecursivePalindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a Palindrome: ");
+            var input = Console.ReadLine();
+
+
+
+            Console.Read();
+        }
+    }
+}
